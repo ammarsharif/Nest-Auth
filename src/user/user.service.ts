2445fd12e07c079615886f7b0585e16a07fafca3
Ammar-Sharif <ammar.sharif.dev@sparkosol.com>
2025-02-28 14:47:48 +0500

    firebase auth

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -11,7 +11,7 @@ export class UserService {
     return this.userModel.findOne({ email }).lean().exec();
   }
   // We use the exec() method to execute the query and return a promise. because the findOne() method returns a Query object, not a promise. we use it with other methods also like find(), findById(), etc.
-  // The lean() method is used to convert the mongoose document into a plain JavaScript object. This method is used to improve the performance of the query. It is used when you don't want to modify the document before sending it to the client.
+  // The lean() method is used to convert the mongoose document into a plain JavaScript object. This method is used to improve the performance of the query. It is used when you don't want to modify the document before sending it to the client. 
 
   async create(user: Partial<User>): Promise<User> {
     const newUser = new this.userModel(user);
